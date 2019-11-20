4696e883011669b02b1b1875b521164771546bbd
louis <louiscahyadi23@gmail.com>
2019-11-21 00:31:44 +0700

    benerin layout home dan transaction

--- src/Container/Transaction/Transaction.jsx
+++ src/Container/Transaction/Transaction.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Transaction.css';
-// import Title from '../../Components/Title/Title';
+import Title from '../../Components/Title/Title';
 import TransactionList from '../../Components/TransactionList/TransactionList';
 
 class Transaction extends React.Component {
     render() {
         return (
             <div className="transaction">
-                {/* <Title title="Transaction"/> */}
-                <div className="judul-transaksi">
+                <Title title="Riwayat Transaksi"/>
+                {/* <div className="judul-transaksi">
                     <h1>Riwayat Transaksi</h1>
-                </div>
+                </div> */}
                 <div className="riwayat-transaksi">
                     <TransactionList waktu="Waktu" jenis="Jenis" jumlah="Jumlah" rekening="Rekening"/>
                     <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
