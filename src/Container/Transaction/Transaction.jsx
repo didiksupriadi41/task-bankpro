6e05107e64b90e8869a1a6d28ba0cbf1e78a5de5
louis <louiscahyadi23@gmail.com>
2019-11-20 15:21:09 +0700

    basic design

--- src/Container/Transaction/Transaction.jsx
+++ src/Container/Transaction/Transaction.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Transaction.css';
 import Title from '../../Components/Title/Title';
 import TransactionList from '../../Components/TransactionList/TransactionList';
 
 class Transaction extends React.Component {
     render() {
         return (
-            <div className="Transaction">
+            <div className="transaction">
                 <Title title="Transaction"/>
-                
+                <div className="judul-transaksi">
+                    <h1>Riwayat Transaksi</h1>
+                </div>
                 <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
                 <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
                 <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
