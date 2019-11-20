4425575afbd899e1e69c821c0fe3192ae2ca8829
louis <louiscahyadi23@gmail.com>
2019-11-20 18:53:57 +0700

    navbar edited

--- src/Container/Transaction/Transaction.jsx
+++ src/Container/Transaction/Transaction.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import './Transaction.css';
-import Title from '../../Components/Title/Title';
+// import Title from '../../Components/Title/Title';
 import TransactionList from '../../Components/TransactionList/TransactionList';
 
 class Transaction extends React.Component {
     render() {
         return (
             <div className="transaction">
-                <Title title="Transaction"/>
+                {/* <Title title="Transaction"/> */}
                 <div className="judul-transaksi">
                     <h1>Riwayat Transaksi</h1>
                 </div>
-                <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
-                <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
-                <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                <div className="riwayat-transaksi">
+                    <TransactionList waktu="Waktu" jenis="Jenis" jumlah="Jumlah" rekening="Rekening"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    {/* <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/> */}
+                    <TransactionList waktu="19-10-2019 17:04:29" jenis="debit" jumlah="300000" rekening="01234567"/>
+                </div>
             </div>
         )
     }
